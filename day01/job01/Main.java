2bc1783fdcc24fb4571695433cb7f8887b21e28f
yoel-settbon <yoel-settbon@laplateforme.io>
2025-06-16 23:12:37 +0200

    :rocket: job01 done

---
+++ day01/job01/Main.java
@@ -0,0 +1,21 @@
+package day01.job01;
+public class Main {
+    public static void main(String[] args) {
+        char lettre = 'A';
+        String message = "Bonjour";
+        int nombre = 42;
+        long grandNombre = 123456789L;
+        float reel = 3.14f;
+        boolean estVrai = true;
+
+        System.out.println("char : " + lettre);
+        System.out.println("String : " + message);
+        System.out.println("int : " + nombre);
+        System.out.println("long : " + grandNombre);
+        System.out.println("float : " + reel);
+        System.out.println("boolean : " + estVrai);
+
+        int TOTO = (int) 3.817;
+        System.out.println("TOTO : " + TOTO);
+    }
+}
